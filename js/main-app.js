53f00c0057e4c64d54589e0cf4ec8d5743d0839b
maximshaa <starwwind@gmail.com>
2025-09-08 06:41:04 +0700

    possibility of transfer to the third and fourth columns, indication of the reason when returning

--- js/main-app.js
+++ js/main-app.js
@@ -4,7 +4,6 @@ Vue.component('columns', {
         <div class="firstColumn" @dragover.prevent @drop="onDrop('firstColumn')">
             <h3>Запланированные задачи</h3>
             <button @click="showCreateModal()">+ создать карточку задачи</button>
-            <modalWindow :show="showModal" :card="currentCard" @close="closeModal" @submit="handleCardSubmit"></modalWindow>
             <div v-for="(card, index) in columns.firstColumn" :key="index" class="card" draggable="true" @dragstart="onDragStart('firstColumn', index)">
                 <h4>{{card.title}}</h4>
                 <p>{{card.description}}</p>
@@ -24,17 +23,38 @@ Vue.component('columns', {
                 <p>Создано: {{card.creationDate}}</p>
                 <p>Дэдлайн: {{card.deadline}}</p>
                 <p v-if="card.lastEdited">Последнее редактирование: {{card.lastEdited}}</p>
+                <p v-if="card.reason">Причина возврата: {{card.reason}}</p>
                 <button @click="showEditModal(card)">Редактировать</button>
             </div>
         </div>
 
-        <div class="thirdColumn">
+        <div class="thirdColumn" @dragover.prevent @drop="onDrop('thirdColumn')">
             <h3>Тестирование</h3>
+            <div v-for="(card, index) in columns.thirdColumn" :key="index" class="card" draggable="true" @dragstart="onDragStart('thirdColumn', index)">
+                <h4>{{card.title}}</h4>
+                <p>{{card.description}}</p>
+                <p>Создано: {{card.creationDate}}</p>
+                <p>Дэдлайн: {{card.deadline}}</p>
+                <p v-if="card.lastEdited">Последнее редактирование: {{card.lastEdited}}</p>
+                <button @click="showEditModal(card)">Редактировать</button>
+            </div>
         </div>
 
-        <div class="fourthColumn">
+        <div class="fourthColumn" @dragover.prevent @drop="onDrop('fourthColumn')">
             <h3>Выполненные задачи</h3>
+            <div v-for="(card, index) in columns.fourthColumn" :key="index" class="card" draggable="true" @dragstart="onDragStart('fourthColumn', index)">
+                <h4>{{card.title}}</h4>
+                <p>{{card.description}}</p>
+                <p>Создано: {{card.creationDate}}</p>
+                <p>Дэдлайн: {{card.deadline}}</p>
+                <p v-if="card.lastEdited">Последнее редактирование: {{card.lastEdited}}</p>
+                <button @click="showEditModal(card)">Редактировать</button>
+            </div>
         </div>
+        
+        <modalWindow :show="showModal" :card="currentCard" @close="closeModal" @submit="handleCardSubmit"></modalWindow>
+        
+        <returnModalWindow :show="showReturnModal" :card="returnReasonCard" @submit="handleReturnCard" @close="closeReturnModal"></returnModalWindow>
     </div>
     `,
 
@@ -51,7 +71,11 @@ Vue.component('columns', {
 
             currentCard: null,
 
-            draggedCard: null
+            draggedCard: null,
+
+            returnReasonCard: null,
+
+            showReturnModal: false
         }
     },
 
@@ -66,14 +90,17 @@ Vue.component('columns', {
 
         onDrop(targetColumn) {
             if (!this.draggedCard || !this.columns[this.draggedCard.column]) return
+            const removedCard = this.columns[this.draggedCard.column].splice(this.draggedCard.index, 1)[0]
 
-            this.columns[this.draggedCard.column].splice(this.draggedCard.index, 1)
-
-            this.columns[targetColumn].push(this.draggedCard.card)
+            if (this.draggedCard.column === 'thirdColumn' && targetColumn === 'secondColumn') {
+                this.returnReasonCard = removedCard
+                this.showReturnModal = true
+            } else {
+                this.columns[targetColumn].push(removedCard)
+                this.saveData()
+            }
 
             this.draggedCard = null
-
-            this.saveData()
         },
 
         showCreateModal() {
@@ -104,6 +131,17 @@ Vue.component('columns', {
             this.showModal = false
         },
 
+        handleReturnCard(card) {
+            this.columns.secondColumn.push(card)
+            this.returnReasonCard = null
+            this.saveData()
+        },
+
+        closeReturnModal() {
+            this.showReturnModal = false
+            this.returnReasonCard = null
+        },
+
         saveData() {
             const data = {
                 columns: this.columns,
@@ -122,6 +160,43 @@ Vue.component('columns', {
 
     created() {
         this.loadData()
+    }
+})
+
+
+
+Vue.component('returnModalWindow', {
+    props: ['show', 'card'],
+    template: `
+    <div v-if="show" class="modal">
+        <div class="modalContent">
+            <h3>Укажите причину возврата</h3>
+            <textarea v-model="returnReason" placeholder="Введите причину"></textarea>
+            <p v-if="errorMessage" class="error">{{errorMessage}}</p>
+            <button @click="submitReturnReason">Сохранить причину</button>
+            <button @click="$emit('close')">Закрыть</button>
+        </div>    
+    </div>`,
+
+    data() {
+        return {
+            returnReason: '',
+            errorMessage: ''
+        }
+    },
+
+    methods: {
+        submitReturnReason() {
+            if(!this.returnReason.trim()) {
+                this.errorMessage = 'Пожалуйста, укажите приину возврата'
+                return
+            }
+
+            this.card.reason = this.returnReason
+            this.$emit('submit', this.card)
+            this.$emit('close')
+            this.errorMessage = ''
+        }
     }
 })
 
@@ -193,12 +268,5 @@ Vue.component('modalWindow', {
 
 
 let app = new Vue({
-    el: '#app',
-    data: {
-
-    },
-
-    methods: {
-
-    },
+    el: '#app'
 })
