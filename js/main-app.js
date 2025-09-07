c75008d31bcc43be59283b5dde08d1f25ca0f06a
maximshaa <starwwind@gmail.com>
2025-09-08 06:04:05 +0700

    transfer to js

--- js/main-app.js
+++ js/main-app.js
@@ -1,102 +1,58 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-        columns: {
-            firstColumn: [],
-            secondColumn: [],
-            thirdColumn: []
-        },
+Vue.component('columns', {
+    template: `
+    <div class="columns">
+        <div class="firstColumn">
+            <h3>Запланированные задачи</h3>
+            <div class="addCard">
+                <h3>Добавление карточки задачи</h3>
+                <input type="text" v-model="newCard.title" placeholder="Заголовок карточки"/>
+                <textarea v-model="newCard.items" placeholder="Введите свою задачу"></textarea>
+                <label>Когда задача должна быть выполнена?: </label>
+                <input type="date"/>
+                <button @click="addCard">Добавить карточку</button>
+            </div>
+        </div>
 
-        newCard: {
-            title: '',
-            items: ''
-        },
+        <div class="secondColumn">
+            <h3>Задачи в работе</h3>
+        </div>
 
-        blockFirstColumn: false,
+        <div class="thirdColumn">
+            <h3>Тестирование</h3>
+        </div>
 
-        selectedSeparator: ''
+        <div class="fourthColumn">
+            <h3>Выполненные задачи</h3>
+        </div>
+    </div>`,
+
+    data() {
+        return {
+            columns: {
+                firstColumn: [],
+                secondColumn: [],
+                thirdColumn: [],
+                fourthColumn: []
+            },
+
+            newCard: {
+
+            },
+        }
     },
 
     methods: {
         addCard() {
-            if (this.blockFirstColumn) {
-                alert('Первый столбец заблокирован, пока одна из карточек второго столбца не будет завершена')
-                return
-            }
-
-            if (this.columns.firstColumn.length < 3) {
-                const itemsArray = this.newCard.items.split(this.selectedSeparator).map(item => {
-                    const trimmedItem = item.trim()
-                    return {
-                        text: trimmedItem.startsWith('!') ? trimmedItem.slice(1) : trimmedItem,
-                        completed: false,
-                        important: trimmedItem.startsWith('!')
-                    }
-                })
-
-                const allFilledItems = itemsArray.every(item => item.text !== '')
-                if (!allFilledItems) {
-                    alert('Нельзя добавлять в задачи пустые значения')
-                    return
-                }
-
-
-                if (itemsArray.length < 3 || itemsArray.length > 5) {
-                    alert('Карточка должна содержать от 3 до 5 задач')
-                    return
-                }
-
-                this.columns.firstColumn.push({
-                    title: this.newCard.title,
-                    items: itemsArray,
-                    completedAt: null
-                })
-
-                this.newCard = {title: '', items: ''}
-                this.saveData()
-            } else {
-                alert('Первый столбец может содержать не более 3 карточек')
-            }
-        },
-
-        updateProgress(column, index) {
-            const card = this.columns[column][index]
-            const completedItems = card.items.filter(item => item.completed).length
-            const progress = (completedItems / card.items.length) * 100
-
-            if (progress === 100) {
-                card.completedAt = new Date().toLocaleString()
-                this.moveCard(column, index, 'thirdColumn')
-
-                if (column === 'secondColumn' && this.columns.secondColumn.length < 5) {
-                    this.blockFirstColumn = false
-                }
-
-            } else if (progress > 50 && column === 'firstColumn') {
-                this.moveCard(column, index, 'secondColumn')
-
-                if (this.columns.secondColumn.length >= 5) {
-                    this.blockFirstColumn = true
-                }
-            }
-
             this.saveData()
         },
 
-        moveCard(fromColumn, cardIndex, toColumn) {
-            if (toColumn === 'secondColumn' && this.columns[toColumn].length >= 5) {
-                alert('Второй столбец может содержать не более 5 карточек')
-                return
-            }
-            const card = this.columns[fromColumn].splice(cardIndex, 1)[0]
-            this.columns[toColumn].push(card)
+        moveCard() {
             this.saveData()
         },
 
         saveData() {
             const data = {
                 columns: this.columns,
-                blockFirstColumn: this.blockFirstColumn,
             }
             localStorage.setItem('notesAppData', JSON.stringify(data))
         },
@@ -106,7 +62,6 @@ let app = new Vue({
             if (savedData) {
                 const parsedData = JSON.parse(savedData)
                 this.columns = parsedData.columns
-                this.blockFirstColumn = parsedData.blockFirstColumn
             }
         }
     },
@@ -114,4 +69,17 @@ let app = new Vue({
     created() {
         this.loadData()
     }
+})
+
+
+
+let app = new Vue({
+    el: '#app',
+    data: {
+
+    },
+
+    methods: {
+
+    },
 })
