39758c8ecfc4b82104ae7c4b7727eec665b67a95
maximshaa <starwwind@gmail.com>
2025-09-08 06:24:12 +0700

    create card fix

--- js/main-app.js
+++ js/main-app.js
@@ -1,11 +1,31 @@
 Vue.component('columns', {
     template: `
     <div class="columns">
-        <column title="Запланированные задачи" :cards="columns.firstColumn"></column>
-        <column title="Задачи в работе" :cards="columns.secondColumn"></column>
-        <column title="Тестирование" :cards="columns.thirdColumn"></column>
-        <column title="Выполненные задачи" :cards="columns.fourthColumn"></column>
-    </div>`,
+        <div class="firstColumn">
+            <h3>Запланированные задачи</h3>
+            <button @click="showModal = true">+ создать карточку задачи</button>
+            <modalWindow :show="showModal" @close="showModal = false" @add-card="addCardToFirstColumn"></modalWindow>
+            <div v-for="card in columns.firstColumn" class="card">
+                <h4>{{card.title}}</h4>
+                <p>{{card.description}}</p>
+                <p>Создано: {{card.creationDate}}</p>
+                <p>Дэдлайн: {{card.deadline}}</p>
+            </div>
+        </div>
+
+        <div class="secondColumn">
+            <h3>Задачи в работе</h3>
+        </div>
+
+        <div class="thirdColumn">
+            <h3>Тестирование</h3>
+        </div>
+
+        <div class="fourthColumn">
+            <h3>Выполненные задачи</h3>
+        </div>
+    </div>
+    `,
 
     data() {
         return {
@@ -14,12 +34,15 @@ Vue.component('columns', {
                 secondColumn: [],
                 thirdColumn: [],
                 fourthColumn: []
-            }
+            },
+
+            showModal: false
         }
     },
 
     methods: {
-        moveCard() {
+        addCardToFirstColumn(card) {
+            this.columns.firstColumn.push(card)
             this.saveData()
         },
 
@@ -46,34 +69,39 @@ Vue.component('columns', {
 
 
 
-Vue.component('column', {
-    props: ['title', 'cards'],
-    template: `
-    <div class="column">
-        <h3>{{title}}</h3>
-        <div v-for="card in cards" class="card">
-             <h4>{{card.title}}</h4>
-             <p>{{card.items}}</p>
-        </div>
-    </div>
-`,
-})
-
-
-
 Vue.component('modalWindow', {
     props: ['show'],
     template: `
     <div v-if="show" class="modal">
         <div class="modalContent">
-            <h3>Добавление карточки задачи</h3>
+            <h3>Создание карточки задачи</h3>
             <input type="text" v-model="newCard.title" placeholder="Заголовок карточки"/>
-            <textarea v-model="newCard.items" placeholder="Введите свою задачу"></textarea>
-            <label>Когда задача должна быть выполнена?: </label>
-            <input type="date"/>
-            <button @click="addCard">Добавить карточку</button>
+            <textarea v-model="newCard.description" placeholder="Введите свою задачу"></textarea>
+            <label>Дэдлайн: </label>
+            <input type="date" v-model="newCard.deadline"/>
+            <button @click="createCard">Создать</button>
+            <button @click="$emit('close')">Закрыть</button>
         </div>
     </div>`,
+
+    data() {
+        return {
+            newCard: {
+                title: '',
+                description: '',
+                deadline: '',
+                creationDate: new Date().toISOString().split('T')[0]
+            }
+        }
+    },
+
+    methods: {
+        createCard() {
+            this.$emit('add-card', {...this.newCard})
+            this.newCard = {title: '', description: '', deadline: '', creationDate: new Date().toISOString().split('T')[0]}
+            this.$emit('close')
+        }
+    }
 })
 
 
